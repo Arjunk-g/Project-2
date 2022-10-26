2e7ce6e12cc65bf92c54d62b7e8ca22ff83ab751
a1exlin <104739337+a1exlin@users.noreply.github.com>
2022-10-26 11:37:50 -0400

    updated the commented code in folders

--- Develop/public/js/logout.js
+++ Develop/public/js/logout.js
@@ -4,11 +4,4 @@
 //     headers: { 'Content-Type': 'application/json' },
 //   });
 
-//   if (response.ok) {
-//     document.location.replace('/login');
-//   } else {
-//     alert(response.statusText);
-//   }
-// };
 
-// document.querySelector('#logout').addEventListener('click', logout);
