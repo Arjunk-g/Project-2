35349d439df5663919817314b1ee48d7ef7a9db3
a1exlin <104739337+a1exlin@users.noreply.github.com>
2022-10-26 13:28:55 -0400

    added comments to the MVC folder structure

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
+// We need to create a connection file that connections all mysql and dotenv. 
+
 // const Sequelize = require('sequelize');
 // require('dotenv').config();
-
-
 // module.exports = sequelize;
