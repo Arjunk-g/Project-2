2121cd7a07ee622d3821168a4e0a20af7933881a
a1exlin <104739337+a1exlin@users.noreply.github.com>
2022-10-26 11:56:26 -0400

    updated commented code

--- Develop/utils/helpers.js
+++ Develop/utils/helpers.js
@@ -3,12 +3,4 @@
 //     const randomNum = Math.random();
 //     let book = "📗";
 
-//     if (randomNum > 0.7) {
-//       book = "📘";
-//     } else if (randomNum > 0.4) {
-//       book = "📙";
-//     }
-
-//     return `<span for="img" aria-label="book">${book}</span>`;
-//   },
 // };
