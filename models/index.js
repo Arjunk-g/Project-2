d376eb89c1e914a6aa52b9ac3418f1074a12fe5f
Bee <xronsredstone@gmail.com>
2022-10-26 14:01:57 -0400

    added base tables to models

--- models/index.js
+++ models/index.js
@@ -1,5 +1,15 @@
-// the model of the mvc, where we keep our data model, and algorithms
+const User = require('./User');
+const HighScores = require('./HighScores');
 
-// const User = require('./User');
+HighScores.hasOne(User, {
+  foreignKey: 'user_id',
+  // When we delete a User, make sure to also delete the associated Library Card.
+  onDelete: 'CASCADE',
+});
 
-// module.exports = { User };
+User.belongsTo(HighScores, {
+  foreignKey: 'user_id',
+});
+
+// We package our two models and export them as an object so we can import them together and use their proper names
+module.exports = { User, HighScores };
