b2b3b83e5893afab09d36ad4b63801117d99f1a2
Bee <xronsredstone@gmail.com>
2022-10-27 13:54:24 -0400

    replaced login failed with logout failed

--- models/HighScores.js
+++ models/HighScores.js
@@ -10,11 +10,9 @@ HighScores.init(
         primaryKey: true,
         autoIncrement: true
       },
-    // This column will store a reference of the `id` of the `user` that owns this Library Card
     user_id: {
       type: DataTypes.INTEGER,
       references: {
-        // This references the `user` model, which we set in `user.js` as its `modelName` property
         model: 'user',
         key: 'user_id',
       },
