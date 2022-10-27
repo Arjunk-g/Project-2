7326a8b360f57b7b7f296a366978788b738e00b3
Bee <xronsredstone@gmail.com>
2022-10-27 10:38:30 -0400

    spun up server

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-// Server File for Project 2
+const express = require('express');
+const controllers = require('./controllers');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 4200;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// turn on controllers
+app.use(controllers);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
