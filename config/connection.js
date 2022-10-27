1f20b1368d88a79c3fcdc03246c36356d68944f6
Bee <xronsredstone@gmail.com>
2022-10-27 10:45:06 -0400

    config conection.js added

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,15 @@
-// We need to create a connection file that connections all mysql and dotenv. 
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-// const Sequelize = require('sequelize');
-// require('dotenv').config();
-// module.exports = sequelize;
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 4200,
+  }
+);
 
-// 5fe5afd58006ceca2df2c3533a7e5f0535000f15
+module.exports = sequelize;
