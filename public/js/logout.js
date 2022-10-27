d2320342174c1017e1d74614b47f3ba8a671a730
Bee <xronsredstone@gmail.com>
2022-10-27 13:41:06 -0400

    cleaned up messy code and added back example ENV file

--- public/js/logout.js
+++ public/js/logout.js
@@ -7,9 +7,8 @@
         if (response.ok) {
           document.location.replace('/');
         } else {
-          alert('Failed to log out.');
+          alert(`Login failed.`);
         }
       };
       
       document.querySelector('#logout').addEventListener('click', logout);
-      
