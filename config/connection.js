76860fc3d7351b94a5153a71d0d4536d069c3a0d
a1exlin <104739337+a1exlin@users.noreply.github.com>
2022-10-31 12:19:45 -0400

    updated

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,13 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-<<<<<<< HEAD
   // "user_db",
   // "root",
   // "",
-=======
 // REPLACE THESE WITH THE VALUES IN THE DOTENV WHEN YOU SEED
 // THEN IMMEDIATLY RETURN TO HOW IT WAS, FOR SOME REASON THAT
 // SEED FILE HATES DOTENV...
->>>>>>> main
+
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
