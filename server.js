ce1708f0e14f4b2a56ded29256f807da61aafeb2
a1exlin <104739337+a1exlin@users.noreply.github.com>
2022-10-31 11:31:31 -0400

    handlebars code updated

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
-
-
 
 const app = express();
 const PORT = process.env.PORT || 4200;
