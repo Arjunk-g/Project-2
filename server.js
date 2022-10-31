4a974b0ad22a7afcce77f6ed3bac0212b9986647
Bee <xronsredstone@gmail.com>
2022-10-30 22:28:01 -0400

    smallFix

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
 const exphbs = require('express-handlebars');
+
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
 
 const app = express();
-const PORT = process.env.PORT || 4200;
-// calling handlebars engine
-const handlebars =require('express-handlebars');
-app.set ('view engine','handlebars');
+const PORT = process.env.PORT || 3001;
+
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// turn on controllers
 app.use(routes);
 
 // turn on connection to db and server
